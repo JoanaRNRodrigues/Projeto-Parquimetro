d06b2fa7fa0deeaffd9432e36508daeca807db95
linaAveiro <94610017+linaAveiro@users.noreply.github.com>
2022-01-09 21:06:54 +0000

    Update Car.cs

--- Parquimetro/Car.cs
+++ Parquimetro/Car.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace Parquimetro
 {
-    public class Car
+    internal class Car
     {
+        public int id;
+        public Time time;
+        public Car(int[] exitTime)
+        {
+            this.id = id+1;
+            this.time = new Time(exitTime);                             //um carro será um objeto com um id e um objeto tempo associado que será a hora e data do fim do estacionamento
+        }                                                               // penso que assim será mais fácil para a parte do administrador
+
     }
 }
