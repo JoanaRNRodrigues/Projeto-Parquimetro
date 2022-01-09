fde5effee9ee9085ab444031dc6e40d74a800000
linaAveiro <94610017+linaAveiro@users.noreply.github.com>
2022-01-09 21:08:51 +0000

    Update Classe MyFynctions.cs

--- Parquimetro/Classe MyFynctions.cs
+++ Parquimetro/Classe MyFynctions.cs
@@ -4,8 +4,6 @@ namespace Parquimetro
 {
     public static class MyFunctions
     {
-
-
         public static void giveChange(double change, double[] coins, int[] stock)
         {
             //Esta função pede o valor de troco e imprime as moedas que iriam cair na máquina assim como o total de troco
@@ -15,8 +13,8 @@ namespace Parquimetro
             //Alterado para ter em consideração o stock
             //Dei um stock bastante elevado para diminuir o risco de ficar a zero
             //A função não corre mais do que uma vez
-            if (change > 0 )                                       //se o valor a dar pela máquina for maior do que 0, vai se verificar que moeda devolver.
-            { 
+            if (change > 0)                                       //se o valor a dar pela máquina for maior do que 0, vai se verificar que moeda devolver.
+            {
                 for (int i = 0; i < coins.Length; i++)                // i é o indice do array das coins
                 {
                     while (change >= coins[i] & stock[i] > 0)
@@ -33,87 +31,7 @@ namespace Parquimetro
 
         }
 
-        /*
-         while (change > 0)                                       //enquanto o valor a dar pela máquina for maior do que 0, vai se verificar que moeda devolver, consoante o stock da máquina)
-        {                                                        //tive que alterar para >= senão não funcionava devidamente
-        if (change >= coins[0] & stockCoins[0] > 0)
-        {
-            Console.WriteLine("2€");
-            change = change - coins[0];
-            stockCoins[0]--;
-        }
-        else if (change >= coins[1] & stockCoins[1] > 0)
-        {
-            Console.WriteLine("1€");
-            change = change - coins[1];
-            stockCoins[1]--;
-        }
-        else if (change >= coins[2] & stockCoins[2] > 0)
-        {
-            Console.WriteLine("50 cents");
-            change = change - coins[2];
-            stockCoins[2]--;
-        }
-        else if (change >= coins[3] & stockCoins[3] > 0)
-        {
-            Console.WriteLine("20 cents");
-            change = change - coins[3];
-            stockCoins[3]--;
-        }
-        else if (change >= coins[4] & stockCoins[4] > 0)
-        {
-            Console.WriteLine("10 cents");
-            change = change - coins[4];
-            stockCoins[4]--;
-        }
-        else if (change >= coins[5] & stockCoins[5] > 0)
-        {
-            Console.WriteLine("5 cents");
-            change = change - coins[5];
-            stockCoins[5]--;
-        }
-        else if (change >= coins[6] & stockCoins[6] > 0)
-        {
-            Console.WriteLine("2 cents");
-            change = change - coins[6];
-            stockCoins[6]--;
-        }
-        else if (change >= coins[7] & stockCoins[7] > 0)
-        {
-            Console.WriteLine("1 cents");
-            change = change - coins[7];
-            stockCoins[7]--;
-        }
-        */
-
-
-        public static int[] Time()
-        {
-
-            DateTime tempoPC = DateTime.Now;
-
-            // dia, ano e mes actual (buscado do computador)
-
-            int day = tempoPC.Day;
-            int month = tempoPC.Month;
-            int year = tempoPC.Year;
-
-
-            // horas e minutos
-
-            int horas = tempoPC.Hour;
-            int minutos = tempoPC.Minute;
-            DateTime dateValue = new DateTime(year, month, day);
-            int weekDay = ((int)dateValue.DayOfWeek);
-
-            int[] time = { tempoPC.Hour, tempoPC.Minute, tempoPC.Day, tempoPC.Month, tempoPC.Year, weekDay };
-
-            return time;
-        }
-
-
-
-
+        
 
         public static string Menu(string title, string[] options)                 //Função que devolve os menus
         {
@@ -148,15 +66,11 @@ namespace Parquimetro
             return MenuType;
         }
 
-
-
-
-
         public static double minutesCount(double change, Zone zone, int[] stock, double[] coins)
         {
-            int[] currentTime = MyFunctions.Time();
+            Time currentTime = new Time();
             double minutesParking;
-            if (change >= zone.MaxChange & zone.MaxChange > 0)
+            if (change >= zone.MaxChange & zone.MaxChange > 0)                      //Se a pessoa inserir mais troco do que o necessário para pagar o tempo máximo da zona 1 ou 2 a máquina dá troco
             {
                 minutesParking = zone.TimeLimit;
                 MyFunctions.giveChange(change - zone.MaxChange, coins, stock);
@@ -164,29 +78,25 @@ namespace Parquimetro
             }
             else
             {
-                minutesParking = (60 * change) / zone.CostPerHour;  //tornar função universal com array que recebe preço e maxchange
+                minutesParking = (60 * change) / zone.CostPerHour;  
                 return minutesParking;
             }
         }
 
-
-
-
-
-        public static void zoneTime(double change, Zone zone, int[] stock, double[] coins)
+        public static int[] zoneTime(double change, Zone zone, int[] stock, double[] coins)
         {
             double parkingMinutes = minutesCount(change, zone, stock, coins);
-            int[] currentTime = MyFunctions.Time();
+            Time currentTime = new Time();
 
-            int currentHour = currentTime[0];
-            int currentMinute = currentTime[1];
+            int currentHour = currentTime.Hour;
+            int currentMinute = currentTime.Minute;
 
             int exitMinute = (int)Math.Round(parkingMinutes) + currentMinute;
             int exitHour = currentHour;
 
-            int exitDay = currentTime[2];
-            int weekDay = currentTime[5];
-            int exitMonth = currentTime[3];
+            int exitDay = currentTime.Day;
+            int weekDay = currentTime.DayOfWeek;
+            int exitMonth = currentTime.Month;
 
             if (exitMinute >= 60)
             {
@@ -196,11 +106,11 @@ namespace Parquimetro
             }
 
 
-            if (exitHour >= 20 & currentTime[5] <= 5 || exitHour >= 14 & currentTime[5] == 6)
+            if (exitHour >= 20 & currentTime.DayOfWeek <= 5 || exitHour >= 14 & currentTime.DayOfWeek == 6)
             {
                 if (zone.id < 2)
                 {
-                    Console.WriteLine("20h00 " + exitDay + "/" + exitMonth + "/" + currentTime[4]); // adaptar ao menu ou alterar o retorno para array
+                    Console.WriteLine("20h00 " + exitDay + "/" + exitMonth + "/" + currentTime.Year); 
                 }
                 else
                 {
@@ -249,7 +159,7 @@ namespace Parquimetro
             }
             else
             {
-                Console.WriteLine("" + exitHour + "h" + exitMinute + " " + exitDay + "/" + exitMonth + "/" + currentTime[4]);
+                Console.WriteLine("" + exitHour + "h" + exitMinute + " " + exitDay + "/" + exitMonth + "/" + currentTime.Year);
             }
             if (zone.id == 2)
             {
@@ -260,10 +170,11 @@ namespace Parquimetro
                     exitMonth += 1;
 
                 }
-                Console.WriteLine("" + exitHour + "h" + exitMinute + " " + exitDay + "/" + exitMonth + "/" + currentTime[4]); // adaptar ao menu ou alterar o retorno para array
+                Console.WriteLine("" + exitHour + "h" + exitMinute + " " + exitDay + "/" + exitMonth + "/" + currentTime.Year); // adaptar ao menu ou alterar o retorno para array
             }
+            int[] exitTime = { currentTime.Year, exitMonth, exitDay, exitHour, exitMinute, 0 };
 
-
+            return exitTime;
         }
 
     }
