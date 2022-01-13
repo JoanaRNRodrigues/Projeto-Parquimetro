c5e7c74319f831fc9067ee56ffe97105da9d9a2d
linaAveiro <94610017+linaAveiro@users.noreply.github.com>
2022-01-13 23:11:52 +0000

    alteração no giveChange para simplificar a faturação e no dayGains

--- Parquimetro/Classe MyFynctions.cs
+++ Parquimetro/Classe MyFynctions.cs
@@ -9,14 +9,14 @@ namespace Parquimetro
     public static class MyFunctions
     {
         public static int id;                                                       // id que identifica os objetos carro
-        public static bool needChange;                                              // variável que descreve se um transação envolveu troco para fins de faturação
+        
         public static double totalGains;                                            // Variável que vai sendo incrementada consuante os valores que são ganhos num dado dia
         public static void giveChange(double change, double[] coins)
         {
             //Esta função pede o valor de troco e imprime as moedas que iriam cair na máquina assim como o total de troco
 
             Console.WriteLine($"Troco: {change} euros");
-
+            totalGains -= change;
             //Alterado para ter em consideração o stock
             //Dei um stock bastante elevado para diminuir o risco de ficar a zero
             //A função não corre mais do que uma vez
@@ -30,7 +30,7 @@ namespace Parquimetro
 
                 }
             }
-
+            
         }
 
         public static string Menu(string title, string[] options)                   //Função que devolve os menus
@@ -76,7 +76,7 @@ namespace Parquimetro
             {
                 minutesParking = zone.TimeLimit;                                    // o valor máximo da zona será assumido e o restante dinheiro devolvido como troco
                 MyFunctions.giveChange(change - zone.MaxChange, coins);
-                needChange = true;
+                
                 return minutesParking;
             }
             else
@@ -86,21 +86,15 @@ namespace Parquimetro
             }
         }
 
-        public static void dayGains(bool needChange, double change, Zone zone)
+        public static void dayGains(double change, Zone zone)
         {
             // esta funcção recebe um booleano que indica se numa transação foi necessário troco, o dinheiro inserido nessa transação e a zona de estacionamento
             // ela incrementa na variável totalGains o dinheiro ganho na transação, para que seja possível consultar a faturação diária
-            if (needChange == false)
-            {
-                totalGains += change;                                               // caso a transação não envolva troco, o dinheiro inserido é adicionado à faturação no seu total
-            }
-            else
-            {
-                totalGains += zone.MaxChange;                                       // caso contrário, o que só acontece nas zonas com tempo limite, 
-            }                                                                       // é adicionado o valor máximo associado à zona pois foi comprado o tempo máximo
+            totalGains += change;                                               
+                                                                                
         }
 
-        public static void exceedTime(Zone[] zones) 
+        public static void exceedTime(Zone[] zones)
         {
             // esta função recebe o parametro zones que é um array das 3 zonas disponiveis
             // determina se existe algum carro a exceder o tempo de estacionamento e notifica o administrador quando seleciona essa opção no menu
@@ -112,7 +106,7 @@ namespace Parquimetro
                     Car car = zone.Spaces[i];
                     if (car != null)
                     {
-                        if (car.parked == true && (car.time.Hour < now.Hour && car.time.Day==now.Day|| (car.time.Hour == now.Hour && car.time.Minute < now.Minute && car.time.Day == now.Day)))
+                        if (car.parked == true && (car.time.Hour < now.Hour && car.time.Day == now.Day || (car.time.Hour == now.Hour && car.time.Minute < now.Minute && car.time.Day == now.Day)))
                         {
                             Console.WriteLine($"O carro no lugar {i} da zona {zone.id} está a exceder o estacionamento");
                         }
