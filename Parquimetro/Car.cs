0a859e40c2f332465531624270649f140d1bd169
linaAveiro <94610017+linaAveiro@users.noreply.github.com>
2022-01-14 00:44:57 +0000

    alteração final com encapsulamento

--- Parquimetro/Car.cs
+++ Parquimetro/Car.cs
@@ -8,28 +8,30 @@ namespace Parquimetro
 {
     public class Car
     {
-        public int id;
-        public Time time;
-        public bool parked;
-        public Car(int[] exitTime)
+        private int id;                          //variavel para o identificador de cada objeto car
+        private Time time;                       //variável do tipo Time que descreve o tempo limite para o estacionamento do carro
+        private bool parked;                     //variável que descreve se o carro está estacionado ou não
+
+        public int ID
         {
-
-            parked = true;
-            this.time = new Time(exitTime);                             //um carro será um objeto com um id e um objeto tempo associado que será a hora e data do fim do estacionamento
-        }                                                               // penso que assim será mais fácil para a parte do administrador
-
-
+            get { return id; }
+            set { id = value; }
+        }
+        public Time Time
+        {
+            get { return time; }
+            set { time = value; }
+        }
+        public bool Parked
+        {
+            get { return parked; }
+            set { parked = value; }
+        }
         public Car(Time time, int id)
         {
+            //Construtor por parametros de objetos da classe Car
             parked = true;
             this.time = time;
         }
-           
-        // penso que assim será mais fácil para a parte do administrador
-        public void exitParking()
-        {
-            this.parked = false;
-        }
-     
     }
 }
